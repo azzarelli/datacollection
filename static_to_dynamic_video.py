4c2f824059ed3f8d7b5a9a7933489a08a439f8dc
azzarelli <aazzarellib@gmail.com>
2023-06-19 15:46:13 +0100

    Check included

--- static_to_dynamic_video.py
+++ static_to_dynamic_video.py
@@ -1,12 +1,18 @@
 import argparse
 import os
 
+TEST_DFP = './data/boat_colmap/'
+TEST_VFP = './data/boat/boat.mp4'
+
 def static_to_dynamic_dataset(d_fp, v_fp):
     """Convert static colmap data to dynamic data such as the data used in D-NeRF
     
     Args:
         d_fp, v_fp: str, data folder and video filepaths respectively 
     """
+    assert os.path.exists(d_fp), 'Data file path does not exist'
+    assert os.path.exists(v_fp), 'Video file path does not exist'
+    assert '.mp4' == v_fp[-4:], 'Need mp4' # TODO: Test on other video formats
 
 
 if __name__ == "__main__":
@@ -21,5 +27,7 @@ if __name__ == "__main__":
                     help='Original video filepath (default: "")')
     
     args = vars(parser.parse_args())
-
-    print(args['data'], args['video'])
+    
+    
+    static_to_dynamic_dataset(TEST_DFP, TEST_VFP)
+    # static_to_dynamic_dataset(args['data'], args['video'])
