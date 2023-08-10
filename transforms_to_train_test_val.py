b4b39729331a0c053583d89603cab2e1df109fb3
Adrian Azzarelli <64833452+azzarelli@users.noreply.github.com>
2023-08-10 14:16:28 +0100

    Initial layout for solution

--- transforms_to_train_test_val.py
+++ transforms_to_train_test_val.py
@@ -2,12 +2,17 @@ import os
 from pathlib import Path
 import json
 import random
+import shutil
+
 source = Path("data/200im_360_view")
 
 file_list = os.listdir(source)
 train_split = 0.9 # Set the dataset split ratios
 test_split = 0.08
 val_split = 0.02
+
+linked_fp_flag = ['_depth', '_normal'] # if images have linked depth and normal renders they'll usually be denotes e.g. as 'r_0_depth.png' or similar
+                                    # We want to move these files as well...
 
 assert (train_split + test_split + val_split == 1.), 'Make sure data split adds up to 1.'
 assert file_list != [], 'File list empty - make sure correct folder is selected.'
@@ -50,34 +55,38 @@ with open(source/file) as fp:
     test_frames = frames[train_idx:test_idx]
     val_frames = frames[test_idx:]
 
-for frame in train_frames:
-    fp_ = train_path / frame['file_path'].split('/')[-1] # append end of file path to new image path
-    frame['file_path'] = fp_ # set new file path
+def move_files(targ_path, frame, file_list):
+    image_id = frame['file_path'].split('/')[-1]
+    fp_ = targ_path / image_id  # append end of file path to new image path
 
+    shutil.move(source/(image_id+'.png'), fp_) # move source image to destination
 
-image_ids = []
-frames = {}
-# Go through the files in the folder to sort
-for file in file_list:
-    # Process the transforms file into 
-    if 'transforms.json' == file:
-        
-        
-        
-        for frame in meta['frames']:
-            frame_fp = frame['file_path'].split('/')[-1] # get the image name
-        print(meta.keys())
-    # Process the images - where depth and normal flags aren't included in the original file name
-    if '.png' in file and 'depth' not in file and 'normal' not in file:
-        id = file.split('_')[1].split('.')[0] # Expecting fp names like r_0.png to turn into str('0')
-        image_ids.append(int(id))
-
-image_ids = sorted(image_ids)
-dataset_size = len(image_ids) # get size of dataset to run dataset split
-
-
-
-print(sorted(image_ids))
-            
+    # Also append linked images if they exist
+    for flag in linked_fp_flag:
+        linked_fp = image_id + flag
+        linked_fp_ = [file_path for file_path in file_list if linked_fp in file_path][0]
+        shutil.move(source/linked_fp, targ_path/linked_fp)
     
+    return fp_
 
+# Process frames for training, testing and validation
+for frame in train_frames:
+    frame['file_path'] = move(train_path, frame, file_list)
+
+for frame in test_frames:
+    frame['file_path'] = move(train_path, frame, file_list)
+    
+for frame in val_frames:
+    frame['file_path'] = move(train_path, frame, file_list)
+
+train_tranform = {'camera_angle_x': meta['camera_angle_x'],
+                    'frames': train_frames
+                 }
+    
+test_tranform = {'camera_angle_x': meta['camera_angle_x'],
+                    'frames': test_frames
+                 }
+
+val_tranform = {'camera_angle_x': meta['camera_angle_x'],
+                    'frames': val_frames
+                 }
